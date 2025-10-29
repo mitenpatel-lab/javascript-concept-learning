672af809deb77c4548d80baaa0610083e35574a2
mitenpatel-lab <miten.patel@mindinventory.com>
2025-10-29 13:16:39 +0530

    implementing array concept

--- basicConcept.js
+++ basicConcept.js
@@ -5,5 +5,17 @@ const fullName=function(){
     return firstName + " " + lastName;
     
 }
-
 console.log(fullName());
+
+//Arrays
+
+let numbers=[67,90,23,12,9,56,89]
+
+console.log(numbers.length)
+
+numbers.length>6?console.log("array elements are greater than 6")
+                :console.log("array elements are less than 6")
+
+numbers.forEach((num)=>{
+    console.log(num);
+});
