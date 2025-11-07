43feadbe2f5abf56f5e90b8a0f47f82984011bd6
mitenpatel-lab <miten.patel@mindinventory.com>
2025-11-07 18:57:00 +0530

    feat: Create flight booking basic funcationality.

--- exam/nov-7/que2.js
+++ exam/nov-7/que2.js
@@ -1,16 +1,29 @@
+// function getstr(s) {
+
+//     let str = s.split(" ");
+
+//     let temp = "";
+
+//     str.forEach(element => {
+
+//         if (element.length > temp.length) {
+//             temp = element;
+//         }
+//     });
+//     return temp;
+// }
+
+// console.log(getstr("hello jay miten"));
+
 function getstr(s) {
 
     let str = s.split(" ");
 
-    let temp = "";
+    str.reduce((largest, current) => {
+        largest.length > current.length ? largest : current;
 
-    str.forEach(element => {
 
-        if (element.length > temp.length) {
-            temp = element;
-        }
-    });
-    return temp;
+    }, "");
 }
 
 console.log(getstr("hello jay miten"));
